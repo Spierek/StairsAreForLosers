c5dff037cac636d16c1d94d79345e4cbbe87ce45
Luke Spierewka <spierek@gmail.com>
2014-12-08 23:04:48 +0100

    [+] Added an invincibility period to player's hitbox [*] Spider projectiles properly disappear after hitting the player

--- unity/Assets/Scripts/Hitbox.cs
+++ unity/Assets/Scripts/Hitbox.cs
@@ -3,22 +3,30 @@
 [RequireComponent(typeof(BoxCollider2D))]
 public class Hitbox : MonoBehaviour {
     #region Variables
-    private PlayerController player;
     private new Collider2D collider2D;
     #endregion
 
     #region Monobehaviour Methods
     private void Awake() {
-        player = transform.parent.GetComponent<PlayerController>();
         collider2D = GetComponent<Collider2D>();
     }
 
     private void OnTriggerEnter2D(Collider2D col) {
         // TODO: add enemy damage
-        player.Hit(1, col.transform.position);
+        PlayerController.Instance.Hit(1, col.transform.position);
+        collider2D.enabled = false;
+        Invoke("EnableCollider", PlayerController.Instance.invincibilityDuration);
+
+        
+        if (col.gameObject.layer == LayerMask.NameToLayer("Projectile")) {
+            col.GetComponent<SpiderProjectile>().Die();
+        }
     }
     #endregion
 
     #region Methods
+    private void EnableCollider() {
+        collider2D.enabled = true;
+    }
     #endregion
 }
