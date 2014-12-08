eb5c6cc6382aa4e01097a2c95e7685ba8fc16710
Bartłomiej Sieczka <retro44@gmail.com>
2014-12-08 23:07:13 +0100

    [*] New entites version

--- unity/Assets/Scripts/Entity.cs
+++ unity/Assets/Scripts/Entity.cs
@@ -9,17 +9,29 @@ public class Entity : MonoBehaviour
     public Vector3 finalPosition;
     public SpriteRenderer spriteRenderer;
     public Transform spriteContainer;
-
+    public bool hurtOnFall;
+    private Transform collider;
+   
     protected virtual void Start()
     {
         spriteRenderer = GetComponentInChildren<SpriteRenderer>();
         spriteContainer = transform.Find("SpriteContainer");
+        if(hurtOnFall)
+        {
+            collider = transform.Find("FallHitbox");
+        }
+        float tweenTime = 0.5f + (delayTween ? 0.25f : 0f);
         if (tweenComponent)
         {
             if (!doNotTween)
             {
                 spriteRenderer.transform.localPosition = new Vector3(0.0f, 1.01f, -4.14f);
-                spriteRenderer.transform.DOMove(finalPosition, 0.5f + (delayTween ? 0.25f : 0f)).OnStart(OnStartFalling).OnComplete(OnFallen).SetEase(Ease.InQuint);
+                spriteRenderer.transform.DOMove(finalPosition, tweenTime).OnStart(OnStartFalling).OnComplete(OnFallen).SetEase(Ease.InQuint);
+                if (hurtOnFall)
+                {
+                    Invoke("OnMidTween", tweenTime / 2);
+                    Invoke("OnEndTween", tweenTime);
+                }
             }
             else
             {
@@ -32,9 +44,26 @@ public class Entity : MonoBehaviour
             if(!doNotTween)
             {
                spriteContainer.localPosition = new Vector3(0.0f, 1.01f, -4.14f);
-               spriteContainer.DOMove(finalPosition, 0.5f + (delayTween ? 0.25f : 0f)).OnStart(OnStartFalling).OnComplete(OnFallen).SetEase(Ease.InQuint);
+               spriteContainer.DOMove(finalPosition, tweenTime).OnStart(OnStartFalling).OnComplete(OnFallen).SetEase(Ease.InQuint);
+               if (hurtOnFall)
+               {
+                   Invoke("OnMidTween", tweenTime / 2);
+                   Invoke("OnEndTween", tweenTime);
+               }
             }
         }
+    }
+
+
+    protected void OnMidTween()
+    {
+        collider.gameObject.SetActive(true);
+
+    }
+
+    protected void OnEndTween()
+    {
+        collider.gameObject.SetActive(false);
     }
 
     protected virtual void OnStartFalling()
