2dbcc3f4d945df76242c44158349ae8dd9c09a24
Luke Spierewka <spierek@gmail.com>
2014-12-09 03:40:18 +0100

    [+] Score

--- unity/Assets/Scripts/Other/HeartDisplay.cs
+++ unity/Assets/Scripts/Other/HeartDisplay.cs
@@ -5,6 +5,7 @@ using UnityEngine.UI;
 public class HeartDisplay : MonoBehaviour {
     #region Variables
     public List<Image> sprites;
+    public Text floor;
     #endregion
 
     #region Monobehaviour Methods
@@ -16,6 +17,8 @@ public class HeartDisplay : MonoBehaviour {
         for (int i = 0; i < 8; i++) {
             sprites[i].enabled = i < PlayerController.Instance.health;
         }
+
+        floor.text = Map.instance.bestFloor.ToString();
     }
     #endregion
 
