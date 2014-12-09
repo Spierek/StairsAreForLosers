8cd0735dfe7e670b5a8056dea2d62d652009872b
Luke Spierewka <spierek@gmail.com>
2014-12-09 02:37:41 +0100

    [+] Temporary title screen (waiting for nice graphics)

---
+++ unity/Assets/Scripts/Other/TitleScreen.cs
@@ -0,0 +1,36 @@
+﻿using DG.Tweening;
+using UnityEngine;
+
+public class TitleScreen : MonoBehaviour {
+    #region Variables
+    private float introDelay = 2f;
+    private float outroDelay = 1.5f;
+    private float timer;
+
+    private bool isOutro;
+
+    private SpriteRenderer sprite;
+    #endregion
+
+    #region Monobehaviour Methods
+    void Awake () {
+        sprite = GetComponent<SpriteRenderer>();
+        sprite.material.color = Color.black;
+        sprite.material.DOColor(Color.white, introDelay);
+    }
+    
+    void Update () {
+        if (!isOutro && timer > introDelay && (Input.GetKeyDown(KeyCode.Return) || Input.GetMouseButtonDown(0))) {
+            isOutro = true;
+            timer = 0;
+            sprite.material.DOColor(Color.black, outroDelay);
+        }
+
+        if (isOutro && timer > outroDelay) {
+            Application.LoadLevel(Application.loadedLevel + 1);
+        }
+
+        timer += Time.deltaTime;
+    }
+    #endregion
+}
